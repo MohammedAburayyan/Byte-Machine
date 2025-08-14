4021ccedc0b18e8c8985b8a06ceaf5f5cb2b2bc0
Mohammed Abu-Rayyan <mohammedaburayyan2206@gmail.com>
2025-08-14 14:01:39 -0400

    Third commit

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 )
 
 type byteMachine struct {
-	memory  []byte
-	stack   []int
-	pointer int
+	memory   []byte
+	stack    []int
+	pointer  int
+	register [3]int
 }
 
 var Bigbytemachine byteMachine
+
+func pop(someStack []int) []int {
+	return someStack[:len(someStack)-1]
+}
+func push(someStack []int, otherStack []int) []int {
+	return append(someStack, otherStack...)
+}
+
+// func store(someregister [3]int, value int, index int) [3]int {
+
+// }
 
 func memorySteps(steps []byte) {
 	Bigbytemachine.memory = steps
@@ -25,8 +37,9 @@ func memorySteps(steps []byte) {
 		if Bigbytemachine.memory[Bigbytemachine.pointer] == 0xf9 { // Addition feature
 			stack := Bigbytemachine.stack
 			addedStack := stack[len(stack)-1] + stack[len(stack)-2]
-			stack = stack[:len(stack)-2]
-			stack = append(stack, addedStack)
+			stack = pop(stack)
+			stack = pop(stack)
+			stack = push(stack, []int{addedStack})
 			Bigbytemachine.stack = stack
 			Bigbytemachine.pointer++
 			continue
@@ -34,8 +47,11 @@ func memorySteps(steps []byte) {
 		if Bigbytemachine.memory[Bigbytemachine.pointer] == 0xf8 { //Subtraction feature
 			stack := Bigbytemachine.stack
 			subtractedStack := stack[len(stack)-1] - stack[len(stack)-2]
-			stack = stack[:len(stack)-2]
-			stack = append(stack, subtractedStack)
+			stack = pop(stack)
+			stack = pop(stack)
+			stack = push(stack, []int{subtractedStack})
+			//stack = stack[:len(stack)-2]
+			//stack = append(stack, subtractedStack)
 			Bigbytemachine.stack = stack
 			Bigbytemachine.pointer++
 			continue
@@ -43,8 +59,9 @@ func memorySteps(steps []byte) {
 		if Bigbytemachine.memory[Bigbytemachine.pointer] == 0xf7 { //Division feature
 			stack := Bigbytemachine.stack
 			dividedStack := stack[len(stack)-2] / stack[len(stack)-1]
-			stack = stack[:len(stack)-2]
-			stack = append(stack, dividedStack)
+			stack = pop(stack)
+			stack = pop(stack)
+			stack = push(stack, []int{dividedStack})
 			Bigbytemachine.stack = stack
 			Bigbytemachine.pointer++
 			continue
@@ -52,16 +69,33 @@ func memorySteps(steps []byte) {
 		if Bigbytemachine.memory[Bigbytemachine.pointer] == 0xf6 { //Modulo feature
 			stack := Bigbytemachine.stack
 			ModuloStack := stack[len(stack)-2] % stack[len(stack)-1]
-			stack = stack[:len(stack)-2]
-			stack = append(stack, ModuloStack)
+			stack = pop(stack)
+			stack = pop(stack)
+			stack = push(stack, []int{ModuloStack})
 			Bigbytemachine.stack = stack
 			Bigbytemachine.pointer++
 			continue
 		}
+		if Bigbytemachine.memory[Bigbytemachine.pointer] == 0xf5 { //store feature
+			index := Bigbytemachine.memory[Bigbytemachine.pointer+1]
+			value := Bigbytemachine.stack[len(Bigbytemachine.stack)-1]
+			Bigbytemachine.register[index] = value
+			Bigbytemachine.stack = pop(Bigbytemachine.stack)
+			Bigbytemachine.pointer += 2
+			fmt.Println("Register: ", Bigbytemachine.register)
+		}
+		if Bigbytemachine.memory[Bigbytemachine.pointer] == 0xf4 { //load feature
+			index := Bigbytemachine.memory[Bigbytemachine.pointer+1]
+			value := Bigbytemachine.register[index]
+			Bigbytemachine.stack = append(Bigbytemachine.stack, value)
+			Bigbytemachine.pointer += 2
+			continue
+		}
 	}
 	fmt.Println("Stack: ", Bigbytemachine.stack)
+
 }
 func main() {
-	steps := []byte{0xff, 0x01, 0xff, 0x04, 0xf6}
+	steps := []byte{0xff, 0x01, 0xf5, 0x00, 0xf4, 0x00}
 	memorySteps(steps)
 }
